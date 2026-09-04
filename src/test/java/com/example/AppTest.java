0b6dea0d46e531886e14211f0a9dc2a014c784a3
rkurapati42-sys <rohithkurapati42@gmail.com>
2026-09-04 16:16:38 +0000

    build: add Maven Java project

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class AppTest {
+
+    @Test
+    void messageShouldBeCorrect() {
+        assertEquals("Hello from GitHub Actions!", App.message());
+    }
+}
